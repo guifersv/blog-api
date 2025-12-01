75c657e5e6abf385a33d7ac327c2731e7f2e9312
Guilherme Fernandes <gui.fer.sv@gmail.com>
2025-12-01 16:26:36 -0300

    Fix inline variables

--- src/Utilities/Utils.cs
+++ src/Utilities/Utils.cs
@@ -7,29 +7,22 @@ public static class Utils
 {
     public static CommentDto CommentModel2Dto(CommentModel commentModel)
     {
-        CommentDto commentDto = new()
-        {
-            Content = commentModel.Content,
-            CreatedAt = commentModel.CreatedAt,
-        };
-        return commentDto;
+        return new() { Content = commentModel.Content, CreatedAt = commentModel.CreatedAt };
     }
 
     public static LikeDto LikeModel2Dto(LikeModel likeModel)
     {
-        LikeDto likeDto = new() { CreatedAt = likeModel.CreatedAt };
-        return likeDto;
+        return new() { CreatedAt = likeModel.CreatedAt };
     }
 
     public static PostDto PostModel2Dto(PostModel postModel)
     {
-        PostDto postDto = new()
+        return new()
         {
             Title = postModel.Title,
             Content = postModel.Content,
             CreatedAt = postModel.CreatedAt,
             UpdatedAt = postModel.UpdatedAt,
         };
-        return postDto;
     }
 }
